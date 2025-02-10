70d9fae9fc4592dda2fbb61966b8bb4fc7dc479a
manaswini pattanaik <133735641+ManaswiniPattanaik898@users.noreply.github.com>
2025-02-10 21:18:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Reverse a String/reverse-a-string.py
+++ Difficulty: Basic/Reverse a String/reverse-a-string.py
@@ -1,24 +1,19 @@
 #User function Template for python3
-
 class Solution:
-     def reverseString(self, s: str) -> str:
-         return s[-1::-1]
-        # code here
-        
+    def revStr (self, s : str) -> str :
+        # code here 
+        return s[-1::-1]
 
 
 #{ 
  # Driver Code Starts
 #Initial Template for Python 3
-
-if __name__ == "__main__":
-    t = int(input())
-    while (t > 0):
+if __name__ == '__main__': 
+    t = int (input ())
+    for _ in range (t):
         s = input()
+        
         ob = Solution()
-        print(ob.reverseString(s))
-        t = t - 1
-
+        print(ob.revStr(s))
         print("~")
-
 # } Driver Code Ends
